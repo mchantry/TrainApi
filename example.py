6ba548ca569fbd5cc79caf5b718bbed889d5c93e
Matthew Chantry <matthew.chantry@physics.ox.ac.uk>
2020-04-21 21:17:33 +0100

    TOC added to example

--- example.py
+++ example.py
@@ -2,8 +2,9 @@ import trainapi
 req = trainapi.getstationpairdate("KGX","CBG","2020/06/14")
 #There's loads of data in here, a combination of dictionaries and lists
 #Printing the object will help illuminate but here's an example
-print("Serices from KGX to CBG")
+print("Services from KGX to CBG")
 for serv in req['services']:
+    toc = serv['atocName']
     ori_time = serv['locationDetail']['origin'][0]['publicTime']
     dep_time = serv['locationDetail']['destination'][0]['publicTime']
-    print("Departs %s, arrives %s"%(ori_time,dep_time))
+    print("%s service, departs %s, arrives %s"%(toc,ori_time,dep_time))
